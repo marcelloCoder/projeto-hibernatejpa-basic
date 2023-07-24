012eb71fcfa994b4dd348514e8173947c6d19ebb
marcelloCoder <deoliveiral767@gmail.com>
2023-07-24 12:57:20 -0300

    Classes marca, carro, acessorios ok

---
+++ src/main/java/br/com/mcoder/HibernateJPATeste/entities/Carro.java
@@ -0,0 +1,83 @@
+package br.com.mcoder.HibernateJPATeste.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_carro")
+public class Carro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "carro_seq")
+	@SequenceGenerator(name="carro_seq", sequenceName="sq_carro", initialValue = 1, allocationSize = 1)
+	private Long id;
+	
+	@Column(name = "modelo", length = 50)
+	private String modelo;
+	
+	@Column(name = "placa", length = 50)
+	private String placa;
+
+	public Carro() {
+		super();
+	}
+
+	public Carro(Long id, String modelo, String placa) {
+		super();
+		this.id = id;
+		this.modelo = modelo;
+		this.placa = placa;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carro other = (Carro) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/br/com/mcoder/HibernateJPATeste/entities/Marca.java
@@ -0,0 +1,72 @@
+package br.com.mcoder.HibernateJPATeste.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_marca")
+public class Marca {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "marca_seq")
+	@SequenceGenerator(name="marca_seq", sequenceName="sq_marca", initialValue = 1, allocationSize = 1)
+	private Long id;
+	
+	@Column(name = "nome", length = 50)
+	private String nome;
+
+	public Marca() {
+		super();
+	}
+
+	public Marca(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Marca other = (Marca) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+		
+
+}
